0a75a5e49b6e0d08c8f3a1bf060e1562f6e7f90e
abanoub-talaat <105149048+abanoub-talaat@users.noreply.github.com>
2025-01-31 20:04:11 +0200

    WIP

--- app/Http/Controllers/Api/LiveEventController.php
+++ app/Http/Controllers/Api/LiveEventController.php
@@ -169,6 +169,7 @@ class LiveEventController extends Controller
             // تحقق من عدد المقاعد المتاحة
             if ($liveEvent->reserved_seats >= $liveEvent->number_of_seats) {
                 DB::rollBack();
+                return 0;
                 return ApiHelper::output('لا تستطيع الحجز الآن لأن كل المقاعد ممتلئة', 0);
             }
 
