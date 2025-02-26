58364c98edee122de6698f5a5b32e2a730984bff
abanoub-talaat <105149048+abanoub-talaat@users.noreply.github.com>
2025-02-26 21:45:30 +0200

    WIP

--- app/Http/Controllers/Api/LiveEventController.php
+++ app/Http/Controllers/Api/LiveEventController.php
@@ -161,7 +161,16 @@ class LiveEventController extends Controller
         $liveEventId = $request->get('liveEvent_id');
 
         DB::beginTransaction();
+        $liveEvent = LiveEvent::findOrFail($liveEventId);
+        if($liveEvent->number_of_seats){
+            
 
+            if($liveEvent->number_of_seats <= $liveEvent->usersAttendee()->count()){
+                
+                return;
+                return ApiHelper::output( 'لا تستطيع الحجز الان لان كل المقاعد مكتملة', 0);
+            }    
+        }
         try {
             // قفل السجل لمنع التداخل أثناء تعديل الحدث
             $liveEvent = LiveEvent::lockForUpdate()->findOrFail($liveEventId);
