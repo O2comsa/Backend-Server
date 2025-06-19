1dbb84c5218646c47538695f4c7f48d284356826
Abanoub Talaat (NoubCode) <38631117+abanoubtalaatr@users.noreply.github.com>
2025-06-19 19:46:19 +0300

    Update SendNotificationToAllUsersJob.php

--- app/Jobs/SendNotificationToAllUsersJob.php
+++ app/Jobs/SendNotificationToAllUsersJob.php
@@ -20,7 +20,7 @@ class SendNotificationToAllUsersJob implements ShouldQueue
     protected $title;
     protected $message;
     protected $users;
-    public $timeout = 120; // أو 300 حسب الحاجة (بالثواني)
+    public $timeout = 300; // أو 300 حسب الحاجة (بالثواني)
 
     public function __construct($users,$title, $message)
     {
